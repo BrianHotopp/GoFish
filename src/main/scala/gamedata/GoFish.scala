103c7404ccb16fede15c0044b6695bf54d25b912
Brian Hotopp <brihoto@gmail.com>
2021-07-23 16:06:23 -0400

    refactored game state functions

--- src/main/scala/gamedata/GoFish.scala
+++ src/main/scala/gamedata/GoFish.scala
@@ -7,62 +7,52 @@ import java.util.UUID
 
 // generic model of a gamestate
 // deck is optional because from the perspective of the players, they don't know what the deck is
-case class GoFish(players: Map[UUID, PlayerData], deck: Option[Deck]) {
-  // represents a game of go fish
-  def getPlayerName(uuid: UUID): Option[String] = {
-    players.get(uuid) match {
-      case Some(player) => Some(player.getName)
-      case _ => None
-    }
-  }
-  def getPlayerScore(uuid: UUID): Option[Int] = {
-    players.get(uuid).map(x=>x.getScore)
-  }
-  def getPlayerNames: List[String] = {
-    players.values.map(x => x.getName).toList
-  }
-  def getPlayers: Map[UUID, PlayerData] = players
-  def playerIds: List[UUID] = {
-    players.keys.toList
-  }
-  def deckSize = deck.size
-  def gameOver: Boolean = players.values.map(x => x.getScore).sum == 13
+case class GoFish(players: List[PlayerData], deck: Option[Deck]) {
+  def deckSize: Int = deck.size
+  def gameOver: Boolean = players.map(x => x.getScore).sum == 13
   def playerHas(uuid: UUID, card: Card): Boolean ={
-    players.get(uuid) match {
+    players.find(x => x.id == uuid) match {
       case Some(player) => player.hasCard(card)
       case None => false
     }
   }
-  def printPlayers: Unit = {
-    players.values.foreach(x=>println(x.summary))
+  def printPlayers(): Unit = {
+    players.foreach(x=>println(x.summary))
   }
-  def shuffle = this.copy(deck=deck.map(x=>x.shuffle()))
+  def shuffle: GoFish = this.copy(deck=deck.map(x=>x.shuffle()))
   def dealToAll(numCards: Int): Option[GoFish] = {
+    val nPlayers: Int = players.size
     val totalCards = numCards*nPlayers
     if(totalCards > deck.size){
+      // can't deal more cards then exist
       None
     }else{
-      val (toDistribute, newDeck) = deck.toList.splitAt(totalCards)
+      // todo handle when there is no deck
+      val (toDistribute, newDeck) = deck.get.toList.splitAt(totalCards)
       val newPlayers = players.zipWithIndex.map(x=>{
-        x._1._1->
-        Player(x._1._2, Deck(
-          for{
-            (card, index) <- toDistribute.zipWithIndex if index % nPlayers == x._2
-          }yield{
-           card
-          }
-        ))
-      }).toMap
-      Some(new GoFish(nPlayers, newPlayers, Deck(newDeck)))
+        x._1.copy(
+          hand=Some(
+            Deck(
+              for{
+                (card, index) <- toDistribute.zipWithIndex if index % nPlayers == x._2
+              }yield{
+                card
+              }
+            )
+          )
+        )
+      })
+      Some(this.copy(players = newPlayers, deck = Some(Deck(newDeck))))
     }
   }
   def drawFromDeck(drawerId: UUID): Either[String, (GoFish, Boolean)] = {
-    players.get(drawerId) match {
+    players.find(x=>x.id == drawerId) match {
       case Some(drawer) => {
         // players who are not the drawer
-        val otherPlayers = players.filter(x => !x._1.equals(drawerId))
+        val otherPlayers = players.filter(x => x.id != drawerId)
         // resulting deck and card from draw action
-        val pullResult = deck.pullFromTop()
+        // todo fix the fact that this can throw
+        val pullResult = deck.get.pullFromTop()
 
         pullResult match {
           case (Some(x), y) => {
@@ -70,7 +60,7 @@ case class GoFish(players: Map[UUID, PlayerData], deck: Option[Deck]) {
             val newPlayer: PlayerData = drawer.giveCard(x)
             val newDeck: Deck = y
             val needed = drawer.hasCardWithRank(x.rank)
-            Right((new GoFish(nPlayers, otherPlayers + (drawerId -> newPlayer), newDeck), needed))
+            Right((this.copy(players = newPlayer::otherPlayers, deck=Some(newDeck)), needed))
           }
           case (None, _) => Left("Can't draw; deck is empty!")
         }
@@ -80,7 +70,7 @@ case class GoFish(players: Map[UUID, PlayerData], deck: Option[Deck]) {
   }
 
   def askForCard(wantedRank: Rank, askerId: UUID, askeeId: UUID): Either[String, (GoFish, Boolean)] = {
-    (players.get(askerId), players.get(askeeId)) match {
+    (players.find(x=>x.id == askerId), players.find(x=>x.id == askeeId)) match {
       case (Some(asker), Some(askee)) => {
         (asker.hasCardWithRank(wantedRank), askee.hasCardWithRank(wantedRank)) match {
           case (true, true) => {
@@ -88,12 +78,12 @@ case class GoFish(players: Map[UUID, PlayerData], deck: Option[Deck]) {
               println(s"wanted rank: $wantedRank")
               askee.takeCards(x => x.rank == wantedRank)
             }
-            val uninvolvedPlayers = players.filter(x => (x._1 != askerId) && (x._1 != askeeId))
+            val uninvolvedPlayers = players.filter(x => (x.id != askerId) && (x.id != askeeId))
             val newAsker = asker.giveCards(haul)
-            val newGameState = new GoFish(nPlayers, uninvolvedPlayers + (askerId -> newAsker) + (askeeId -> newAskee), deck)
+            val newGameState = this.copy(players = newAskee :: newAsker :: uninvolvedPlayers)
             Right((newGameState, true))
           }
-          case (true, false) => Right((new GoFish(nPlayers, players, deck), false))
+          case (true, false) => Right((this, false))
           case (false, _) => Left("You must have at least one card of the rank you are asking for!")
         }
       }
@@ -107,22 +97,10 @@ case class GoFish(players: Map[UUID, PlayerData], deck: Option[Deck]) {
     // construct new players with scores, ranks and hands newly created
     // construct delta group for info passing
     // idempotent
-    // how do I write this without triple mapping
-    val groupResult = players.map(x=>x._1 -> x._2.makeGroups)
-    val newPlayers = groupResult.map(x=> x._1->x._2._1)
-    val delta = groupResult.map(x=>x._1->x._2._2)
+    // todo this can be done with one map
+    val (newPlayers, delta) = players.map(x=>x.makeGroups).map(x=>(x._1, x._1.id -> x._2)).unzip
     // construct new gamedata.GoFish
-    (new GoFish(nPlayers, newPlayers, deck), delta)
-  }
-}
-object GoFish {
-  // default constructor
-  def apply(nPlayers: Int = 2, names: List[String] = List("Brian", "Kiara")): GoFish = {
-    val uuids: List[UUID] = List.fill(nPlayers)(UUID.randomUUID())
-    val pairs = for(
-      n <- 0 to nPlayers-1
-    ) yield(uuids(n), Player(names(n)))
-    new GoFish(nPlayers, pairs.toMap, Deck())
+    (this.copy(players = newPlayers), delta.toMap)
   }
 }
 
